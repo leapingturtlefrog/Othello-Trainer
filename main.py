a45d773a0740998deead849bfffad6d915768c30
Alex Aridgides <Alexanderaridgides@gmail.com>
2023-12-28 10:08:12 -0500

    qww

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#Play
+#AI
+#create puzzles
+
 #empty square: 2
 #white piece: 1
 #black piece: 0
@@ -14,14 +18,21 @@ board = [2, 2, 2, 2, 2, 2, 2, 2,
 #black pieces, white pieces
 score = [2, 2]
 
+#moves executed, 0 = game has not started, 60 = game has definitely ended
+move = 0
+turn = 'Black'
+
 #calculate current score, but do not display it
+#input: none, output: sum of pieces on board, changed: score variable
 def calcScore():
+    '''auto doc string'''
     blackScore = 0
     whiteScore = 0
     for q in board:
         if q == 0:
-            blackScore = blackScore + 1
+            blackScore += 1
         elif q == 1:
-            whiteScore = whiteScore + 1
+            whiteScore += 1
+    global score
     score = [blackScore, whiteScore]
-    
+    return blackScore + whiteScore
